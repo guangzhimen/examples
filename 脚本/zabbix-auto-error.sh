af8ce6a004184e1c28237151281a0e18cc5da1ae
guangzhimen <564891512@qq.com>
2018-01-24 02:37:27 +0000

    Update zabbix-auto-error.sh

--- 脚本/zabbix-auto-error.sh
+++ 脚本/zabbix-auto-error.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #######zabbix auto error handle #########
-value=$(mysql -u ansible -paxon@123 -h 10.10.188.210 -Ne "select * from zabbix.alerts order by alertid desc limit 100;" |grep "test-141.38 is unreachable for 5 minutes" |wc -l)
+value=$(mysql -u xxxx -pxxxx -h 10.10.188.210 -Ne "select * from zabbix.alerts order by alertid desc limit 100;" |grep "test-141.38 is unreachable for 5 minutes" |wc -l)
    
    if [ $value -ge 1 ];then      
    /usr/bin/ansible-playbook /etc/ansible/playbooks/test.yml > /dev/null 2>1&
-   mysql -u ansible -paxon@123 -h 10.10.188.210 -Ne "delete from zabbix.alerts where message like '%test-141.38 is unreachable for 5 minutes%';"
+   mysql -u xxxx -pxxxx -h 10.10.188.210 -Ne "delete from zabbix.alerts where message like '%test-141.38 is unreachable for 5 minutes%';"
 fi
